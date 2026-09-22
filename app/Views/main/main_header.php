8ed0e5b58204dc7cc5dd406297915aae0e948d36
jbestar <jbestar1105@gmail.com>
2026-09-23 00:12:22 +0800

    상단메뉴바 폰트 30% 증가

--- app/Views/main/main_header.php
+++ app/Views/main/main_header.php
@@ -13,7 +13,7 @@
         <link rel="stylesheet" href="/assets/css/lib/jquery-ui.css">
         <link rel="stylesheet" href="/assets/css/lib/all.css">
 
-        <link rel="stylesheet" href="/assets/css/main.css?v=12">
+        <link rel="stylesheet" href="/assets/css/main.css?v=13">
         <link rel="stylesheet" href="/assets/css/button1.css">
         <link rel="stylesheet" href="/assets/css/button2.css">
         <link rel="stylesheet" href="/assets/css/simplePagination.css">        
